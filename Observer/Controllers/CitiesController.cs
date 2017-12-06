275f0a5f7fb746fe7f5f3757fd85f27d80945a71
Kondratkov <xaratyan1@mail.ru>
2017-12-06 11:55:39 +0300

    b

---
+++ Observer/Controllers/CitiesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Observer;
+
+namespace Observer.Controllers
+{
+    public class CitiesController : Controller
+    {
+        private ObserverDBEntities db = new ObserverDBEntities();
+
+        // GET: Cities
+        public ActionResult Index()
+        {
+            var cities = db.Cities.Include(c => c.Region);
+            return View(cities.ToList());
+        }
+
+        // GET: Cities/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cities cities = db.Cities.Find(id);
+            if (cities == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cities);
+        }
+
+        // GET: Cities/Create
+        public ActionResult Create()
+        {
+            ViewBag.Region_id = new SelectList(db.Region, "Id", "Name");
+            return View();
+        }
+
+        // POST: Cities/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Region_id,Latitude,Longitude")] Cities cities)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Cities.Add(cities);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Region_id = new SelectList(db.Region, "Id", "Name", cities.Region_id);
+            return View(cities);
+        }
+
+        // GET: Cities/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cities cities = db.Cities.Find(id);
+            if (cities == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Region_id = new SelectList(db.Region, "Id", "Name", cities.Region_id);
+            return View(cities);
+        }
+
+        // POST: Cities/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Region_id,Latitude,Longitude")] Cities cities)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cities).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Region_id = new SelectList(db.Region, "Id", "Name", cities.Region_id);
+            return View(cities);
+        }
+
+        // GET: Cities/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cities cities = db.Cities.Find(id);
+            if (cities == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cities);
+        }
+
+        // POST: Cities/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Cities cities = db.Cities.Find(id);
+            db.Cities.Remove(cities);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
